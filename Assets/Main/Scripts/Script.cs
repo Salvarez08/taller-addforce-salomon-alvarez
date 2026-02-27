d9dea3d67291a3e7ec2764bcdf50c4b4ecad4ac4
Salvarez08 <salvarez565@soyudemedellin.edu.co>
2026-02-26 20:54:28 -0500

    Fuerza Random
    Le añadí fuerza random para que no siempre cayera en la misma parte, parece funcionar, aunque aún así lo veo muy similar, incluso cuando pongo valores muy altos de fuerza.(preguntarle al profe)

--- Assets/Main/Scripts/Script.cs
+++ Assets/Main/Scripts/Script.cs
@@ -8,20 +8,23 @@ public class Script : MonoBehaviour
     //Declarar Variables
     [SerializeField] private TMP_Text _saludo;
     [SerializeField] private Rigidbody2D _pelotica;
-    [SerializeField] private float _fuerza = 10f;
+    [SerializeField] private float _minForce = 10f;
+    [SerializeField] private float _maxForce = 5000f;
     [SerializeField] private string _nombre = "Salomón";
     [SerializeField] private int _edad = 18;
-    [SerializeField] private int _añoactual = 2026;
-    [SerializeField] private int _añonacimiento;
-   
+    [SerializeField] private int _añoActual = 2026;
+    [SerializeField] private int _añoNacimiento;
+    [SerializeField] private float _randomMagnitude;
+
     //Start
 
     void Start()
     {
-        _añonacimiento = _añoactual - _edad;
+        _añoNacimiento = _añoActual - _edad;
+        _randomMagnitude = Random.Range(_minForce, _maxForce);
 
-        _saludo.text = "Hola, mi nombre es " + _nombre + " y nací en " + _añonacimiento;
+        _saludo.text = "Hola, mi nombre es " + _nombre + " y nací en " + _añoNacimiento;
 
-        _pelotica.AddForce(Vector2.right * _fuerza, ForceMode2D.Impulse);
+        _pelotica.AddForce(Vector2.right * _randomMagnitude, ForceMode2D.Impulse);
     }
 }
